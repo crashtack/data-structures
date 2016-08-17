fe8d706dbe23b25a7481fb20bda4dfb6fb455f18
James <canning.jimmy@gmail.com>
2016-08-17 14:07:37 -0700

    compleated basic stack and implemented feedback on linked list. All tests passing.

--- src/stack.py
+++ src/stack.py
@@ -2,7 +2,15 @@
 from linked_list import LinkedList
 
 
-class Stack(LinkedList):
+class Stack(object):
 
     def __init__(self, input=None):
-        pass
+        self._linked_list = LinkedList(input)
+
+    def pop(self):
+        """pops the vlaue of the top of the stack"""
+        return self._linked_list.pop()
+
+    def push(self, val):
+        """pushes a value onto the top of the stack"""
+        return self._linked_list.push(val)
