2f89682355df0280ff557d30fca591c1bfa7778e
crashtack <crashtack@gmail.com>
2016-08-22 15:38:39 -0700

    testing methods

--- src/deque.py
+++ src/deque.py
@@ -6,40 +6,38 @@ from doubly_linked_list import Node
 class Deque(DoubleLinkedList):
 
     def __init__(self, iterable=None):
-        self.front = None
-        self.end = None
+        self.head = None
+        self.tail = None
         if iterable is not None:
             for i in iterable:
-                self.push(i)
+                self.appendleft(i)
 
     def peek(self):
-        if self.end is None:
+        if self.tail is None:
             return None
         else:
-            return self.end.data
+            return self.tail.data
 
     def peekleft(self):
-        if self.front is None:
+        if self.head is None:
             return None
         else:
-            return self.front.data
+            return self.head.data
 
     def size(self):
         """Returns the number of items in the list"""
-        current = self.front
+        current = self.head
         count = 0
         while current:
             count += 1
             current = current.next_node
         return count
 
-    def popleft(self, arg):
-        super(Deque, self).pop(arg)
+    def popleft(self):
+        super(Deque, self).pop()
 
-    def pop(self, arg):
-        super(Deque, self).shift(arg)
+    def pop(self):
+        return super(Deque, self).shift()
 
     def appendleft(self, arg):
         super(Deque, self).push(arg)
-
-
--- src/test_deque.py
+++ src/test_deque.py
@@ -1,2 +1,90 @@
 # -*- coding utf-8 -*-
 import pytest
+
+ITERABLE_TABLE = [
+    None,
+    ['string'],
+    'strings are itearble',
+    (4, 45, 6, 7),
+    [4, 5, 6, 4, 3, 5],
+]
+#
+# @pytest.fixture
+# def deque_with_vals():
+#     from queue_ import Queue
+#     q_with_vals = Queue(['sam', 'tim', 'tom'])
+#     return q_with_vals
+
+
+def test_deque():
+    from deque import Deque
+    assert 0 == 0
+
+
+def test_deque_empty():
+    from deque import Deque
+    d = Deque()
+    assert d.size() == 0
+
+
+@pytest.mark.parametrize('iterable', ITERABLE_TABLE)
+def test_deque_initialize(iterable):
+    from deque import Deque
+    d = Deque(iterable)
+    try:
+        assert d.size() == len(iterable)
+    except TypeError:
+        assert d.size() == 0
+
+
+@pytest.mark.parametrize('iterable', ITERABLE_TABLE)
+def test_deque_append(iterable):
+    from deque import Deque
+    d = Deque(iterable)
+    d.append('something')
+    assert d.tail.data == 'something'
+
+
+def test_deque_size():
+    from deque import Deque
+    d = Deque()
+    assert d.size() == 0
+
+
+@pytest.mark.parametrize('iterable', ITERABLE_TABLE)
+def test_deque_appendleft(iterable):
+    from deque import Deque
+    d = Deque(iterable)
+    d.appendleft('something')
+    assert d.head.data == 'something'
+
+
+@pytest.mark.parametrize('iterable', ITERABLE_TABLE)
+def test_deque_appendleft_twice(iterable):
+    from deque import Deque
+    d = Deque(iterable)
+    d.appendleft('something')
+    d.appendleft(43)
+    assert d.head.data == 43
+
+
+@pytest.mark.parametrize('iterable', ITERABLE_TABLE)
+def test_deque_pop(iterable):
+    from deque import Deque
+    d = Deque(iterable)
+    if iterable:
+        assert d.pop() == iterable[0]
+    else:
+        with pytest.raises(IndexError):
+            d.pop()
+
+
+@pytest.mark.parametrize('iterable', ITERABLE_TABLE)
+def test_deque_popleft(iterable):
+    from deque import Deque
+    d = Deque(iterable)
+    if iterable:
+        assert d.popleft() == iterable[-1]
+    else:
+        with pytest.raises(IndexError):
+            d.popleft()
