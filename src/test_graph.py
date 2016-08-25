f7dd0f74193d7c3cc419ad589fc509041e07f9d3
crashtack <crashtack@gmail.com>
2016-08-25 14:04:10 -0700

    shit!

--- src/test_graph.py
+++ src/test_graph.py
@@ -8,8 +8,32 @@ INIT_TABLE = [
 
 
 @pytest.mark.parametrize('init_nodes, init_edges', INIT_TABLE)
-def test_graph_constructor(init_nodes, init_edges):
+def test_graph_constructor_nodes_no_edges(init_nodes, init_edges):
     '''DOCSTRING'''
     from graph import Graph
     g = Graph(init_nodes)
+    assert g.nodes == init_nodes
+
+
+@pytest.mark.parametrize('init_nodes, init_edges', INIT_TABLE)
+def test_graph_constructor_nodes_edges(init_nodes, init_edges):
+    '''DOCSTRING'''
+    from graph import Graph
+    g = Graph(init_nodes, init_edges)
+    assert g.nodes == init_nodes
+
+
+@pytest.mark.parametrize('init_nodes, init_edges', INIT_TABLE)
+def test_graph_constructor_edges(init_nodes, init_edges):
+    '''DOCSTRING'''
+    from graph import Graph
+    g = Graph(init_nodes, init_edges)
+    assert g.edges == init_edges
+
+
+@pytest.mark.parametrize('init_nodes, init_edges', INIT_TABLE)
+def test_graph_nodes(init_nodes, init_edges):
+    '''DOCSTRING'''
+    from graph import Graph
+    g = Graph(init_nodes, init_edges)
     assert g.nodes == init_nodes
