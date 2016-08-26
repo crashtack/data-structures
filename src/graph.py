67b458a1cc9b3ca901248b7aef4fc3222159d905
James <canning.jimmy@gmail.com>
2016-08-25 18:19:43 -0700

    Half way through all tests passing

--- src/graph.py
+++ src/graph.py
@@ -3,28 +3,68 @@
 
 class Graph(object):
 
-    def __init__(self, node_initial=None, edge_initial=None):
-        '''Initialize a Grap with known Nodes and Edges
-            We are expecting node_iterable, and edge_iterable to be
-            lists:
-                    node_initial = [n1, n2, n3....]
-                    edge_initial = [(n1,n2), (n3, n1)....]
-            *** If you want a graph of one node that is a list, put
-                that list in a list: [[list your stuff here]]
-            '''
-        self.nodes = []
-        self.edges = []
+    def __init__(self, initial_graph=None):
+        '''Pass in a graph in the form of a dictionary with the nodes as
+        key, and a list of edges as value, for example
+        {1:[2, 3]},
+         2:[]},
+         3:[1]}
+        '''
 
-        if node_initial is not None:
-            self.nodes = node_initial
+        if initial_graph is None:
+            self.graph = {}
+        else:
+            self.graph = initial_graph
 
-        if edge_initial is not None:
-            self.edges = edge_initial
+    def __iter__(self):
+        return iter(self.graph)
 
     def nodes(self):
-        '''returns a list of all nodes'''
-        return self.nodes
+        '''return a list of all nodes in the graph'''
+        return self.graph.keys()
 
     def edges(self):
         '''returns a list of tuples, that are edges'''
-        return self.edges
+        return self.graph.values()
+
+    def add_node(self, n):
+        '''takes node and adds it to the graph'''
+        if n in self.graph:
+            raise ValueError('Node already exists please try again')
+        else:
+            self.graph[n] = []
+
+    def add_edge(self, n1, n2):
+        '''adds a new edge to the graph connecting ‘n1’ and ‘n2’, if either n1 or n2 are not already
+         present in the graph, they are added.'''
+        if n2 not in self.graph:
+            self.graph[n2] = []
+        if n1 in self.graph:
+            self.graph[n1].append(n2)
+        else:
+            self.graph[n1] = [n2]
+
+    def del_node(self, n):
+        print(n in self.graph.keys())
+        print(self.graph.keys())
+
+        if n in self.graph.keys():
+            self.graph.pop(n)
+        else:
+            raise ValueError('That node does not exist')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test_graph.py
+++ src/test_graph.py
@@ -1,39 +1,153 @@
 # -*- coding utf-8 -*-
 import pytest
 
-INIT_TABLE = [
-    ([[12, 43], 'two', 2, {'two': 2}, ("two", 2)],
-     [([12, 43], 2), ('two', 2), ('two', {'two': 2}), (("two", 2), 'two')]),
-]
+
+TABLE = [(
+    {1: [2, 3],
+     2: [],
+     3: [1]},
+    [1, 2, 3],
+    [[2, 3], [], [1]]
+)]
 
 
-@pytest.mark.parametrize('init_nodes, init_edges', INIT_TABLE)
-def test_graph_constructor_nodes_no_edges(init_nodes, init_edges):
-    '''DOCSTRING'''
+@pytest.fixture
+def initial_graph():
+    graph = {1: [2, 3],
+             2: [],
+             3: [1]}
+    return graph
+
+
+@pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_constructor(initial_graph, nodes_result, edges_result):
+    '''Test graph constructor if graph is passed in'''
     from graph import Graph
-    g = Graph(init_nodes)
-    assert g.nodes == init_nodes
+    g = Graph(initial_graph)
+    assert g.graph == initial_graph
 
 
-@pytest.mark.parametrize('init_nodes, init_edges', INIT_TABLE)
-def test_graph_constructor_nodes_edges(init_nodes, init_edges):
-    '''DOCSTRING'''
+def test_graph_constructor_none():
+    '''Test graph constructor if nothing is passed in''' 
     from graph import Graph
-    g = Graph(init_nodes, init_edges)
-    assert g.nodes == init_nodes
+    g = Graph()
+    assert g.graph == {}
 
 
-@pytest.mark.parametrize('init_nodes, init_edges', INIT_TABLE)
-def test_graph_constructor_edges(init_nodes, init_edges):
-    '''DOCSTRING'''
+@pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_node(initial_graph, nodes_result, edges_result):
+    '''Test node returns a list of nodes'''
     from graph import Graph
-    g = Graph(init_nodes, init_edges)
-    assert g.edges == init_edges
+    g = Graph(initial_graph)
+    assert list(g.nodes()) == nodes_result
 
 
-@pytest.mark.parametrize('init_nodes, init_edges', INIT_TABLE)
-def test_graph_nodes(init_nodes, init_edges):
-    '''DOCSTRING'''
+def test_graph_node_no_graph():
+    '''Test node returns a list of nodes'''
     from graph import Graph
-    g = Graph(init_nodes, init_edges)
-    assert g.nodes == init_nodes
+    g = Graph()
+    assert list(g.nodes()) == []
+
+
+@pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_edges(initial_graph, nodes_result, edges_result):
+    '''Test node returns a list of nodes'''
+    from graph import Graph
+    g = Graph(initial_graph)
+    assert list(g.edges()) == edges_result
+
+
+def test_graph_edges_no_graph():
+    '''Test node returns a list of nodes'''
+    from graph import Graph
+    g = Graph()
+    assert list(g.edges()) == []
+
+
+@pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_add_node(initial_graph, nodes_result, edges_result):
+    '''Test add_node adds nodes to existing graph'''
+    from graph import Graph
+    g = Graph(initial_graph)
+    g.add_node(4)
+    assert 4 in g
+
+
+def test_graph_add_node_empty_graph():
+    '''Test add_node adds nodes to empty graph'''
+    from graph import Graph
+    g = Graph()
+    g.add_node(4)
+    assert 4 in g
+
+
+@pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_add_node_duplicate(initial_graph, nodes_result, edges_result):
+    '''Test add_node adds nodes to existing graph'''
+    from graph import Graph
+    g = Graph(initial_graph)
+    with pytest.raises(ValueError):
+        g.add_node(3)
+
+
+@pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_add_edge_existing_node(initial_graph, nodes_result, edges_result):
+    '''Test add_edge adds an edge'''
+    from graph import Graph
+    g = Graph(initial_graph)
+    g.add_edge(2, 3)
+    assert g.graph[2] == [3]
+
+
+@pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_add_edge_new_node(initial_graph, nodes_result, edges_result):
+    '''Test add_edge adds an edge when a new node is passed'''
+    from graph import Graph
+    g = Graph(initial_graph)
+    g.add_edge(5, 2)
+    assert g.graph[5] == [2]
+
+
+@pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_add_edge_new_edge_node(initial_graph, nodes_result, edges_result):
+    '''Test add_edge adds an edge when a new node is passed as the edge'''
+    from graph import Graph
+    g = Graph(initial_graph)
+    g.add_edge(5, 6)
+    assert g.graph[6] == []
+
+
+@pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_add_edge_new_node_and_new_edge_node(initial_graph, nodes_result, edges_result):
+    '''Test add_edge adds an edge when a new node is passed as the edge'''
+    from graph import Graph
+    g = Graph(initial_graph)
+    g.add_edge(6, 7)
+    assert g.graph[6] == [7]
+
+
+@pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_del_node(initial_graph, nodes_result, edges_result):
+    '''Test del_node deletes a node'''
+    from graph import Graph
+    g = Graph(initial_graph)
+    g.del_node(2)
+    assert 2 not in g
+
+
+# @pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+def test_graph_del_node_not_in_graph(initial_graph):
+    '''Test del_node deletes a node'''
+    from graph import Graph
+    g = Graph(initial_graph)
+    with pytest.raises(ValueError):
+        g.del_node(5)
+
+
+# @pytest.mark.parametrize('initial_graph, nodes_result, edges_result', TABLE)
+# def test_graph_del_node_not_in_graph(initial_graph, nodes_result, edges_result):
+#     '''Test del_node deletes a node'''
+#     from graph import Graph
+#     g = Graph(initial_graph)
+#     with pytest.raises(KeyError):
+#         g.del_node(5)
