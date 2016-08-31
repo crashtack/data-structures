1cd448e7eb06d864467c63755697d422648480da
crashtack <crashtack@gmail.com>
2016-08-31 16:11:33 -0700

    dft and bft tests are passing

--- src/test_graph_travers.py
+++ src/test_graph_travers.py
@@ -17,31 +17,40 @@ TABLE = [(
     [[2, 3], [], [1]]
 )]
 
+GRAPHS = [
+    ({1: []}, [1], [1]),
+    ({1: [2], 2: [3], 3: [1]}, [1, 2, 3], [1, 2, 3])
+    # ({1: [2, 3], 2: [4, 5], 3: [], 4: [], 5: []},),
+    # ({1: [2], 2: [3], 3: [1]},),
+]
 
-@pytest.fixture
-def g():
+
+@pytest.fixture(params=GRAPHS)
+def g(request):
     from graph import Graph
-    GRAPH2 = {1: [2, 3],
-              2: [4, 5],
-              3: [],
-              4: [],
-              5: []}
-    g = Graph(GRAPH2)
-    return g
+    dict_ = request.param[0]
+    graph = Graph(dict_)
+    return (graph, dict_, request.param[1], request.param[2])
 
 
 def test_graph_constructor(g):
     '''Test graph constructor if graph is passed in'''
-    assert g.graph == GRAPH2
+    g1 = g[0]
+    dict_ = g[1]
+    assert g1.graph == dict_
 
 
 def test_dft(g):
     '''Test depth first traversal'''
-    print('\n\ng: {}'.format(g.nodes()))
-    assert g.depth_first_traversal(1) == [1, 3, 2, 5, 4]
+    g1 = g[0]
+    dft = g[2]
+    print('\n\ng: {}'.format(g1.nodes()))
+    assert g1.depth_first_traversal(1) == dft
 
 
 def test_bft(g):
     '''Test breadth first traversal'''
-    print('\n\ng: {}'.format(g.nodes()))
-    assert g.breadth_first_traversal(1) == [1, 2, 3, 4, 5]
+    g1 = g[0]
+    bft = g[3]
+    print('\n\ng: {}'.format(g1.nodes()))
+    assert g1.breadth_first_traversal(1) == bft
