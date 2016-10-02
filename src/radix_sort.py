c78b7615d91a29a705a50f74fd421b52c110d4ed
crashtack <crashtack@gmail.com>
2016-10-02 13:43:29 -0700

    switching to stack

--- src/radix_sort.py
+++ src/radix_sort.py
@@ -1,14 +1,32 @@
 from math import floor
+from stack import Stack
 
 
-def radix(ll):
+def radix(l1):
     '''radix sort'''
-    digit = len(str(max(ll)))
+    digit = len(str(max(l1)))
     dict_ = {}
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     while digit:
-        for item in ll:
+        for i in range(10):
+            dict_[i] = Stack()
+        # import pdb; pdb.set_trace()
+
+        # load the up dict_
+        for item in l1:
             if digit == 1:
                 num = (floor(item / digit) % 10)
             else:
                 num = (floor(item / (digit - 1) ** 10) % 10)
+            dict_[num].push(item)
+
+        # empty the dict_ back into the list
+        l1 = []
+        for i in range(10):
+            while dict_[i].size():
+                l1.append(dict_[i].pop())
+
+        digit -= 1
+    # import pdb; pdb.set_trace()
+
+    return l1
